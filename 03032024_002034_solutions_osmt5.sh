2a9313fb116230a1a0e154564b0b7ef78e0fedca
qveqveriaqo <beriasesili@gmail.com>
2024-03-03 00:20:34 +0000

    Updating solutions

---
+++ 03032024_002034_solutions_osmt5.sh
@@ -0,0 +1,1 @@
+When Black Jack Ketchum was hung back in 1901 in Clayton New Mexico, the noose actually ended up taking his head off. The head had to be sewn back on so Black Jack could be buried properly
